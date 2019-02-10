f1a11faaffbc9688f6090b45c09725a24741d94c
Rahul_Goti <gotirahul1@gmail.com>
2019-02-10 21:18:11 +0530

    Added router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-routing';
+
+  routes = [
+    { linkName: 'Home', url: 'home' },
+    { linkName: 'Settings', url: 'settings' }
+  ];
+
 }
